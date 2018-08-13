289bf8796adbbbdaa94bf8f9d72df6faf3cefcdf
Drehling <5mz-mnl@pool348pc05.edu.inform.hs-hannover.de>
2018-08-13 11:30:59 +0200

    Polylines yo

--- Sommer2018/CgSceneGraph/CgPolyline.h
+++ Sommer2018/CgSceneGraph/CgPolyline.h
@@ -1,11 +1,38 @@
 #ifndef CGPOLYLINE_H
 #define CGPOLYLINE_H
 
+#include "CgBase/CgBasePolyline.h"
 
-class CgPolyLine
+
+class CgPolyline:public CgBasePolyline
 {
 public:
-    CgPolyLine();
+    CgPolyline(unsigned int id, glm::vec3 start, glm::vec3 end, glm::vec3 color, unsigned int lineWidth);
+    CgPolyline(unsigned int id, std::vector<glm::vec3> verticies, unsigned int lineWidth);
+    ~CgPolyline();
+
+    const std::vector<glm::vec3>& getVertices() const;
+    glm::vec3 getColor() const;
+    unsigned int getLineWidth() const;
+    void setColor(glm::vec3 color);
+
+
+    Cg::ObjectType getType() const;
+    unsigned int getID() const;
+
+private:
+
+    std::vector<glm::vec3> m_verticies;
+    glm::vec3 m_color;
+    unsigned int m_lineWidth;
+
+
+    const Cg::ObjectType m_type;
+    const unsigned int m_id;
+
 };
+
+inline Cg::ObjectType  CgPolyline::getType() const {return m_type;}
+inline unsigned int CgPolyline::getID() const {return m_id;}
 
 #endif // CGPOLYLINE_H
